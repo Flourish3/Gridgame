b009403406d176a9bcf29781945b81bd379da5f1
Johan Tillmar <Johan Tillmar>
2016-10-06 22:34:42 +0200

    changes in agent

--- agent.py
+++ agent.py
@@ -1,8 +1,19 @@
 class agent():
 	xPos = 0
 	yPos = 0
-	reward = 0
 	damaged = 0
+	isTreasure = 0
 
-	def __init__(self):
+	reward = 0
+	grid = 0
+	controller = 0
+	def __init__(self,grid,controller):
+		self.grid = grid
+		self.controller = controller
 		print("Agent created")
+
+	def step(self):
+		print("Step")
+
+	def getReward(self):
+		return self.reward

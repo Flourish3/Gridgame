ba7ee12baabbbdca889f1491bfcd1a82e007125b
Johan Tillmar <Johan Tillmar>
2016-11-01 19:15:59 +0100

    Cleanup

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from grid import grid
-from controller import controller
+from gridQController import gridQController
 from agent import agent
+import matplotlib.pyplot as plt
 
 gridSize = 5
 
 grid = grid(gridSize)
-controller = controller(gridSize)
+controller = gridQController(gridSize)
 agentSmith = agent(grid,controller)
 
 iterations = 200000
+
+rewards = [0]*iterations
+
 for i in range(0,iterations):
 	agentSmith.step()
 	if i > 100000:
 		controller.setGreed(1)
+	rewards[i] = agentSmith.getReward()
 
+plt.plot(rewards)
+plt.show()
 print("Total reward: " +  str(agentSmith.getReward()) + " Iterations: " + str(iterations) + " Success rate: " + str(agentSmith.getReward()/iterations))
